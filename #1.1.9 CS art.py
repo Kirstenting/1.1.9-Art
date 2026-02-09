6e25babcfdd6e8bc7d6ae39989be17c45cce36ae
Kirstenting <kirstent1929@gmail.com>
2026-02-09 10:36:49 -0800

    Add files via upload

---
+++ #1.1.9 CS art.py
@@ -0,0 +1,16 @@
+#1.1.9 Art
+import turtle as trtl
+#draw base of the house
+trtl.pensize(5)
+trtl.color("black")
+trtl.fillcolor("tan")
+trtl.begin_fill()
+trtl.forward(200)
+trtl.left(90)
+trtl.forward(200)
+trtl.left(90)
+trtl.forward(200)
+trtl.left(90)
+trtl.forward(200)
+trtl.end_fill()
+#draw roof of the house
